41566362d7b9657f1da17288ddccbb799d32fd49
BorisGautier <tchoukouahaboris@outlook.fr>
2022-05-14 09:37:18 +0100

    update test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,13 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('our_village');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'our_village app is running!'
-    );
-  });
 });
