fbc4d74496c88ed062d17171f7a0c24eec1bdacc
BorisGautier <tchoukouahaboris@outlook.fr>
2022-05-14 16:17:14 +0100

    add tranlator & ngbootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,30 +5,55 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutsModule } from './layouts/layouts.module';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './core/auth/interceptors/auth.interceptor';
 import { HttpBaseUrlInterceptor } from './core/interceptors/http/http-base-url.interceptor';
 import { HttpBaseHeaderInterceptor } from './core/interceptors/http/http-base-header.interceptor';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { MultiTranslateHttpLoader } from 'ngx-translate-multi-http-loader';
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new MultiTranslateHttpLoader(httpClient, [
+    { prefix: './assets/i18n/', suffix: '.json' },
+  ]);
+}
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule,MapModule, LayoutsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MapModule,
+    LayoutsModule,
+    NgbModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'fr',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+  ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
-      multi: true
+      multi: true,
     },
     {
       provide: HTTP_INTERCEPTORS,
       useClass: HttpBaseHeaderInterceptor,
-      multi: true
+      multi: true,
     },
     {
       provide: HTTP_INTERCEPTORS,
       useClass: HttpBaseUrlInterceptor,
-      multi: true
-    }
+      multi: true,
+    },
   ],
   bootstrap: [AppComponent],
 })
